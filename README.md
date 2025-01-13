7fc997c9b1fd7148b58b239c796cf4118bcca0a7
nagaishikazuya <>
2025-01-13 17:59:49 +0900

    no message

--- README.md
+++
@@ -1,3 +0,0 @@
-# nagakazu-test
-
-test
---
+++ counter.js
@@ -0,0 +1,21 @@
+(() => {
+    const $counter = document.getElementById("js-counter");
+
+    const clickHandler = (e) => {
+        const $targetButton = e.currentTarget;
+        let currentCount = parseInt($counter.textContent);
+        if($targetButton.textContent === "+"){
+            $counter.textContent = currentCount + 1;
+        }else if($targetButton.textContent === "-"){
+            $counter.textContent = currentCount - 1;
+        }else if($targetButton.textContent === "+10"){
+            $counter.textContent = currentCount +10;
+        }else{
+            $counter.textContent = currentCount -10;
+        }
+    }
+    
+    for(let index = 0; index < document.getElementsByClassName("js-button").length;index++){
+        document.getElementsByClassName("js-button")[index].addEventListener("click",(e) => clickHandler(e))
+    }
+})();
